1861c4d07880d438aff38c3e132316ae53f4b22f
umanghere <umangbansal1st@gmail.com>
2025-02-20 21:14:45 +0530

    Updated allowedOrigins in server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",  // Local development
-  "https://your-frontend.vercel.app"  // Future deployed frontend
+  "https://team-services.vercel.app/"  // Future deployed frontend
 ];
 
 // Middleware
